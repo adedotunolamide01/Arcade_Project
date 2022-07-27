f18d35145416f4815adafc8fdf998171323d0109
adedotun olamide <adedotuntoyese1@gmail.com>
2022-07-27 23:29:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".container"),
+  ShowPw = document.querySelectorAll(".ShowPw"),
+  pwFields = document.querySelectorAll(".password"),
+  signUp = document.querySelector(".signup-link"),
+  login = document.querySelector(".login-link");
+
+//   js code to show/hide password and change icon
+ShowPw.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    pwFields.forEach((pwField) => {
+      if (pwField.type === "password") {
+        pwField.type = "text";
+
+        ShowPw.forEach((icon) => {
+          icon.classList.replace("uil-eye-slash", "uil-eye");
+        });
+      } else {
+        pwField.type = "password";
+
+        ShowPw.forEach((icon) => {
+          icon.classList.replace("uil-eye", "uil-eye-slash");
+        });
+      }
+    });
+  });
+});
+
+// js code to appear signup and login form
+signUp.addEventListener("click", () => {
+  container.classList.add("active");
+});
+login.addEventListener("click", () => {
+  container.classList.remove("active");
+});
